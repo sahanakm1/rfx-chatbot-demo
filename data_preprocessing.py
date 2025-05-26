74ab6915544c64a2ca92e742a1fc8c791ce082fe
prerit-accenture <prerit.mbaiim@gmail.com>
2025-05-26 13:14:00 +0530

    data preprocessing using docling

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,7 @@
 import json
 import logging
 from pathlib import Path
+import pandas as pd
 
 from langchain_community.document_loaders import PyMuPDFLoader
 from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
@@ -12,7 +13,21 @@ from docling.document_converter import (
 )
 from docling.pipeline.simple_pipeline import SimplePipeline
 from docling.pipeline.standard_pdf_pipeline import StandardPdfPipeline
+from langchain_text_splitters import MarkdownHeaderTextSplitter
 from typing import List
+
+
+headers_to_split_on = [
+    ("#", "Header 1"),
+    ("##", "Header 2"),
+    ("###", "Header 3"),
+]
+
+def md_chunk_strat(headers_to_split_on,markdown_document):
+    markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on)
+    md_header_splits = markdown_splitter.split_text(markdown_document)
+    return md_header_splits
+    
 
 doc_converter = (
         DocumentConverter(  # all of the below is optional, has internal defaults.
@@ -25,6 +40,7 @@ doc_converter = (
                 InputFormat.ASCIIDOC,
                 InputFormat.CSV,
                 InputFormat.MD,
+                InputFormat.XLSX,
             ],  # whitelist formats, non-matching files are ignored.
             format_options={
                 InputFormat.PDF: PdfFormatOption(
@@ -38,7 +54,25 @@ doc_converter = (
     )
 
 
+def doc_converter_for_excel(conv_res,output_dir,conv_file_names,md_tables):
+    doc_filename = conv_res.input.file.stem
 
+    # Export tables
+    for table_ix, table in enumerate(conv_res.document.tables):
+        table_df: pd.DataFrame = table.export_to_dataframe()
+        #print(f"## Table {table_ix}")
+        #print(table_df.to_markdown())
+
+        with (output_dir / f"{doc_filename}-table-{table_ix + 1}.md").open("w",encoding="utf-8") as fp:
+            fp.write(table_df.to_markdown())
+        
+        conv_file_names.append(f"{doc_filename}-table-{table_ix + 1}.md")
+
+        chunks = md_chunk_strat(headers_to_split_on,table_df.to_markdown())
+        for c in chunks:
+            md_tables.append(c)
+    
+    return conv_file_names,md_tables
 
 class data_preprocessing:
     def __init__(self, file_path):
@@ -55,9 +89,13 @@ class DocumentConverter:
         self.input_path = input_path
 
     def convert_pdf_to_md(self):
+       
        conv_results = doc_converter.convert_all(self.input_path)
 
-       output_files = []
+       conv_file_names=[]
+       md_docs=[]
+
+       
        for res in conv_results:
           out_path = Path("Scratch")
           out_path.mkdir(parents=True, exist_ok=True)
@@ -65,12 +103,16 @@ class DocumentConverter:
              f"Document {res.input.file.name} converted."
              f"\nSaved markdown output to: {out_path!s}"
           ) 
-          try:
-             with(out_path / f"{res.input.file.stem}.md").open("w") as fp:
-                fp.write(res.document.export_to_markdown())
-          except:
-             with(out_path / f"{res.input.file.stem}.md").open("w",encoding="utf-8") as fp:
-                fp.write(res.document.export_to_markdown())
 
-          output_files.append(out_path / f"{res.input.file.stem}.md")
-       return output_files
+          if res.input.file.name.endswith(".xlsx"):
+             conv_file_names,md_docs = doc_converter_for_excel(res,out_path,conv_file_names,md_docs)
+          else:
+              with (out_path / f"{res.input.file.stem}.md").open("w",encoding = "utf-8") as fp:
+                 fp.write(res.document.export_to_markdown())
+              conv_file_names.append(f"{res.input.file.stem}.md")
+        
+              chunks = md_chunk_strat(headers_to_split_on,res.document.export_to_markdown())
+              for c in chunks:
+                 md_docs.append(c)
+
+       return md_docs
